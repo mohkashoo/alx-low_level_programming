db8d545a38072d1fc66167a2504c9ff9d5e2008c
mohkashoo <kash00m00n@outlook.com>
2023-08-10 11:10:21 -0700

    bye

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 fwrite("and that piece of art is useful\"", sizeof(char), 43, stdout);
-fwrite("2015-10-19\n", sizeof(char), 12, stdout);
-return (0);
+fwrite("2015-10-19\n", sizeof(char), 16, stdout);
+return (1);
 }
