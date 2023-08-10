c4fbba4673b3f8c2901bd6d8631317c1526f2322
mohkashoo <kash00m00n@outlook.com>
2023-08-10 11:02:00 -0700

    bye

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(char), 59, stdout);
-	return (1);
+fwrite("and that piece of art is useful\"", sizeof(char), 43, stdout);
+fwrite("2015-10-19\n", sizeof(char), 12, stdout);
+return (0);
 }
