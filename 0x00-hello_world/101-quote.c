a41dda33943ff06483b8ae766ddaebf374cb6a36
mohkashoo <kash00m00n@outlook.com>
2023-08-10 10:21:31 -0700

    hardthings

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Success)
+ */
+int main()
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ,sizeof(char), 70, stdout);
+    return (1);
+}
