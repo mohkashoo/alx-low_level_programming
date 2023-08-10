e027fd4d9c7f3da5249bb75f06b068111be0f312
mohkashoo <kash00m00n@outlook.com>
2023-08-10 10:27:10 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  *
  * Return: 1 (Success)
  */
-int main()
+int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ,sizeof(char), 70, stdout);
-    return (1);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , sizeof(char), 59 , stdout);
+	return (1);
 }
