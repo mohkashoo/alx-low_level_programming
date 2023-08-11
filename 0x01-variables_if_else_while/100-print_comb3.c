03150d5cd025f037249b21b2d53502de8e849e2a
mohkashoo <kash00m00n@outlook.com>
2023-08-11 15:21:01 -0700

    byegit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -print two digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int first_digit;
+
+	int second_digit;
+
+	for (first_digit = 0; first_digit < 9; first_digit++)
+
+	{
+
+	for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
+
+	{
+
+	putchar(first_digit + '0');
+
+	putchar(second_digit + '0');
+
+	if (first_digit != 8 || second_digit != 9)
+
+	{
+
+	putchar(',');
+
+	putchar(' ');
+
+	}
+
+	}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
