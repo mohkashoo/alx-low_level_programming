0e7b200f8ea5c18f2f8442fe245df53c3b9042f7
mohkashoo <kash00m00n@outlook.com>
2023-08-11 16:19:24 -0700

    up

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,38 +8,34 @@
  */
 
 int main(void)
-
 {
+int num1, num2, num3;
 
-        int first_digit;                                                                
-        int second_digit;
+	for (num1 = 0; num1 <= 7; num1++)
+	{
 
-        for (first_digit = 0; first_digit < 9; first_digit++)
+	for (num2 = num1 + 1; num2 <= 8; num2++)
+	{
 
-        {
+	for (num3 = num2 + 1; num3 <= 9; num3++)
+	{
 
-        for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
+	putchar(num1 + '0');
+	putchar(num2 + '0');
+	putchar(num3 + '0');
 
-        {
+	if (num1 != 7 || num2 != 8 || num3 != 9)
+	{
 
-        putchar(first_digit + '0');
+	putchar(',');
+	putchar(' ');
 
-        putchar(second_digit + '0');
+	}
+	}
+	}
+	}
 
-        if (first_digit != 8 || second_digit != 9)
+	putchar('\n');
+return (0);
 
-        {
-
-        putchar(',');
-
-        putchar(' ');
-
-        }
-
-        }
-
-        }
-
-        putchar('\n');
-
-	return (0);
+}
