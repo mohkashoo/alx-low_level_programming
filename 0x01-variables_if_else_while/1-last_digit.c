9543043e092e18aff24e405f13a58570c28eaf49
mohkashoo <kash00m00n@outlook.com>
2023-08-11 10:11:35 -0700

    do

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,8 +25,7 @@ printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 
 else
 
-printf("Last digit of %d is %d and is less than 6 and not \n0", n, lastDigit);
-
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 return (0);
 
 }
