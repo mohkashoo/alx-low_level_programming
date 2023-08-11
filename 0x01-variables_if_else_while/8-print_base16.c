48202f495006274b69e34564bf3867bfb16b7a76
mohkashoo <kash00m00n@outlook.com>
2023-08-11 14:12:18 -0700

    doit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - hexdicimal
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+
+	{
+
+	putchar(digit);
+
+	}
+
+	for (digit = 'a'; digit <= 'f'; digit++)
+
+	{
+
+	putchar(digit);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
