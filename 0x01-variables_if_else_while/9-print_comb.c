6ea214e08d87cc11ffae85e007498d050176fcd7
mohkashoo <kash00m00n@outlook.com>
2023-08-11 14:35:05 -0700

    makeit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print number followd by commer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+
+	putchar(digit + '0');
+
+	if (digit != 9)
+	{
+
+	putchar(',');
+
+	putchar(' ');
+
+	}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
