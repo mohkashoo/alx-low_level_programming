a94e5089b8d714eef5fa9538f9ea134da0ac7b25
mohkashoo <kash00m00n@outlook.com>
2023-08-15 16:45:56 -0700

    dome

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_jack_bauer - Prints every minute of the day of Jack Bauer.
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
  *
  * This function prints the time from 00:00 to 23:59 using the _putchar
  */
