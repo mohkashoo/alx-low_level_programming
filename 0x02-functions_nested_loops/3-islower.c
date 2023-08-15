f449c2ab622cc367453555df9f2a56517854676e
mohkashoo <kash00m00n@outlook.com>
2023-08-15 12:09:32 -0700

    go

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - to check if lower
+ *
+ * Return : Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+
+
+	if (c)
+	
+        _putchar('1');
+	
+	else
+    	
+	_putchar('0');
+	
+
+	return 0;
+}
