bfb1daf427350c6eb92bd23cf53d9f3650babad1
mohkashoo <kash00m00n@outlook.com>
2023-08-15 17:18:37 -0700

    gi

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ *
+ * This function takes two integers 'a' and 'b' as input, adds them together,
+ * and returns the result.
+ *
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of 'a' and 'b'.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#include "main.h"
+b#include "main.h"
 
 /**
  * times_table - Prints the 9 times table.
