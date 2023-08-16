7652edd9cba5ab06f19dc38c6e224759cf7bf016
mohkashoo <kash00m00n@outlook.com>
2023-08-15 19:21:02 -0700

    bys

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,21 +4,39 @@
 /**
  * print_to_98 - Prints all natural numbers from n to 98.
  *
- * This function prints all natural numbers from the provided integer 'n'
- * separated by a comma and a space, using the standard library functions.
+ * This function prints all natural numbe
+ * separated by a comma and a space, using
  *
  * @n: The starting number.
  */
+
 void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i >= 98; i--) {
+	if (n <= 98)
+	{
+	for (i = n; i <= 98; i++)
+	{
 	printf("%d", i);
 	if (i != 98)
-
-        printf(", ");
-    }
+	{
+	printf(", ");
+	}
+	}
+	}
+	else
+	{
+	for (i = n; i >= 98; i--)
+	{
+	printf("%d", i);
+	if (i != 98)
+	{
+	printf(", ");
+	}
+	}
+	}
 
 	printf("\n");
 }
+
