b2d14c0844a3d5d465957565d5b4d83ce5a80a1b
mohkashoo <kash00m00n@outlook.com>
2023-09-04 05:14:44 -0700

    goup

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,12 +16,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	mee = malloc(sizeof(int*) *height);
+	mee = malloc(sizeof(int *) * height);
 	if (mee == NULL)
 		return (NULL);
 	for (x = 0; x < height; x++)
 	{
-		mee[x] = malloc(sizeof(int) *width);
+		mee[x] = malloc(sizeof(int) * width);
 		if (mee[x] == NULL)
 		{
 			for (; x >= 0; x--)
