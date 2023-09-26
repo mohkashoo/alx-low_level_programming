a3db93ea1ad80070e27b26555dc0ba9212e87834
mohkashoo <kash00m00n@outlook.com>
2023-09-26 06:40:41 -0700

    go

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *listint_len - element number as return
+ *@h: listint_t linked type
+ *
+ *Return: nodes nuber
+ */
+
+size_t listint_len(const listint_t *j)
+
+{
+	size_t ber = 0;
+
+	while (j)
+
+	{
+		ber++;
+
+		j = j->next;
+	}
+
+	return (ber);
+
+}
