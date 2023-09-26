9a529a477757329d37dd03d9adcbfdcb4c334a93
mohkashoo <kash00m00n@outlook.com>
2023-09-26 08:58:21 -0700

    up

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - gave lnked list space
+ *@head: for a spaced ptr
+ */
+
+void free_listint2(listint_t **head)
+
+{
+	listint_t *temp;
+
+	if (head == NULL)
+
+		return;
+
+	while (*head)
+
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+
+}
+
