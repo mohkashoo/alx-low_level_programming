2d34a893e3f8cf589e2f949977c496169219b584
mohkashoo <kash00m00n@outlook.com>
2023-09-26 08:29:15 -0700

    go

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ *add_nodeint - fresh node adding
+ *n: node inseted
+ *@head: the first node
+ *
+ *Return: Null or node ptr
+ */
+
+listint_t* add_nodeint(listint_t **head, const int n)
+
+{
+	listint_t *fresh;
+
+	fresh = malloc(sizeof(listint_t));
+
+	if (!fresh)
+
+		return (NULL);
+
+	fresh->n = n;
+
+	fresh->next = *head;
+
+	*head = fresh;
+
+	return (fresh);
+
+}
