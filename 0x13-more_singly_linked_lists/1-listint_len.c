d20c2e74458607335d50126c77b755e3fcb1ae63
mohkashoo <kash00m00n@outlook.com>
2023-09-26 06:44:08 -0700

    fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,9 +2,9 @@
 
 /**
  *listint_len - element number as return
- *@h: listint_t linked type
+ *@j: listint_t linked type
  *
- *Return: nodes nuber
+ *Return: nodes number
  */
 
 size_t listint_len(const listint_t *j)
